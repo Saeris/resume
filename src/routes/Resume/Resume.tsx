a67dca1919f9da6a0737dae43e5f0b567c0458e9
Drake Costa <drake@saeris.io>
2022-06-18 21:32:28 -0700

    feat: Add PWA support, Use `getStaticProps` for resume data

--- src/routes/Resume/Resume.tsx
+++ src/routes/Resume/Resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import { Icons } from "./Icons";
 import { Logo } from "./Logo";
 import { Job } from "./Job";
@@ -31,71 +31,72 @@ import {
   Note
 } from "./elements";
 
-export const Resume: NextPage = () => {
-  const { data } = results;
-  return (
-    <Main>
-      <Navigation>
-        <Fullname>{data.fullName}</Fullname>
-        <Title>{data.title}</Title>
-        <Contact>
-          {data.contacts.map(({ label, to }) => (
-            // @ts-expect-error
-            <Service key={label} label={label} to={to}>
-              {Icons[label]} {to.replace(`https://www.`, ``)}
-            </Service>
+export const getStaticProps: GetStaticProps<{ data: typeof results["data"] }> = () => ({
+  props: results
+});
+
+export const Resume: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ data }) => (
+  <Main>
+    <Navigation>
+      <Fullname>{data.fullName}</Fullname>
+      <Title>{data.title}</Title>
+      <Contact>
+        {data.contacts.map(({ label, to }) => (
+          // @ts-expect-error
+          <Service key={label} label={label} to={to}>
+            {Icons[label]} {to.replace(`https://www.`, ``)}
+          </Service>
+        ))}
+      </Contact>
+    </Navigation>
+    <Content>
+      <Summary>
+        <Header>Summary:</Header>
+        <Break />
+        <Text>{data.summary}</Text>
+      </Summary>
+      <Expertise>
+        <Header>Expertise:</Header>
+        <Break />
+        <Skills>
+          {data.skills.map((skill) => (
+            <Skill key={skill}>{skill}</Skill>
           ))}
-        </Contact>
-      </Navigation>
-      <Content>
-        <Summary>
-          <Header>Summary:</Header>
-          <Break />
-          <Text>{data.summary}</Text>
-        </Summary>
-        <Expertise>
-          <Header>Expertise:</Header>
-          <Break />
-          <Skills>
-            {data.skills.map((skill) => (
-              <Skill key={skill}>{skill}</Skill>
-            ))}
-          </Skills>
-        </Expertise>
-        <Experience>
-          <Header>Experience:</Header>
-          <Break />
-          <List>
-            {data.jobs.map((props) => (
-              <Job key={`${props.role}-${String(props.company)}`} {...props} />
-            ))}
-          </List>
-          <Note>Additional Experience Available Upon Request</Note>
-        </Experience>
-        <Projects>
-          <Header>Projects:</Header>
-          <Break />
-          <List>
-            {data.projects.map((props) => (
-              <Project key={props.name} {...props} />
-            ))}
-          </List>
-        </Projects>
-        <Education>
-          <Header>Education:</Header>
-          <Break />
-          <Schools>
-            {data.schools.map((props) => (
-              <School key={props.name} {...props} />
-            ))}
-          </Schools>
-        </Education>
-      </Content>
-      <Footer>
-        <Link title="Saeris.io - Home" href="/">
-          <Logo />
-        </Link>
-      </Footer>
-    </Main>
-  );
-};
+        </Skills>
+      </Expertise>
+      <Experience>
+        <Header>Experience:</Header>
+        <Break />
+        <List>
+          {data.jobs.map((props) => (
+            <Job key={`${props.role}-${String(props.company)}`} {...props} />
+          ))}
+        </List>
+        <Note>Additional Experience Available Upon Request</Note>
+      </Experience>
+      <Projects>
+        <Header>Projects:</Header>
+        <Break />
+        <List>
+          {data.projects.map((props) => (
+            <Project key={props.name} {...props} />
+          ))}
+        </List>
+      </Projects>
+      <Education>
+        <Header>Education:</Header>
+        <Break />
+        <Schools>
+          {data.schools.map((props) => (
+            <School key={props.name} {...props} />
+          ))}
+        </Schools>
+      </Education>
+    </Content>
+    <Footer>
+      <Link title="Saeris.io - Home" href="/">
+        <Logo />
+      </Link>
+    </Footer>
+  </Main>
+);
