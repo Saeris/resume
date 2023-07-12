7414b176f892b71070baaa765c19ea4dfed85deb
Drake Costa <drake@saeris.io>
2023-07-12 03:25:20 -0700

    fix metadata

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  compiler: {
-    styledComponents: true
-  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {
   Titillium_Web as titillium,
   Orbitron as orbitron
 } from "next/font/google";
+import type { Metadata } from "next/types";
 import "../theme/reset.css";
 import "../theme/theme.css";
 
@@ -17,19 +18,53 @@ const display = orbitron({
   variable: "--font-orbitron",
   subsets: ["latin"]
 });
-/* eslint-enable @typescript-eslint/quotes */
 
-const meta = {
-  title: `Drake Costa - Full-Stack JavaScript Engineer`,
-  description: `Resume`,
-  locale: `en_US`,
-  url: `https://saeris.io`,
-  twitter: `@saeris`,
-  favicon: `/favicon.ico`,
-  image: `/share-card.webp`,
-  manifest: `/manifest.json`,
-  color: `#127cbb`
+const title = "Drake Costa - Resume";
+const description =
+  "Full-Stack TypeScript Engineer specializing in Design Systems and Developer Experience.";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  creator: "Drake Costa",
+  authors: [{ name: "Drake Costa", url: "https://saeris.gg" }],
+  manifest: "/manifest.json",
+  themeColor: "#127cbb",
+  openGraph: {
+    title,
+    description,
+    url: "https://resume.saeris.gg",
+    siteName: title,
+    images: [
+      {
+        url: "/share-card.webp",
+        width: 1200,
+        height: 628
+      }
+    ],
+    locale: "en-US",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@saeris",
+    images: ["/share-card.webp"]
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+    apple: [
+      { url: "/logo192.png", sizes: "192x192", type: "image/png" },
+      { url: "/logo512.png", sizes: "512x512", type: "image/png" }
+    ]
+  },
+  appleWebApp: {
+    title,
+    statusBarStyle: "black-translucent"
+  }
 };
+/* eslint-enable @typescript-eslint/quotes */
 
 const Root = ({ children }: { children: React.ReactNode }) => (
   <html lang="en" className={[body.variable, display.variable].join(` `)}>
